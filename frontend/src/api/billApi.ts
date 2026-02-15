d51cfa3fd2bd9ee0bd46f0cd5d1cb4b3bf39d4a1
Trần Công Tính <165116050+Hnit04@users.noreply.github.com>
2026-02-15 19:52:56 +0700

    billmanager basic

---
+++ frontend/src/api/billApi.ts
@@ -0,0 +1,26 @@
+import axiosClient from "./axiosClient";
+import type { Bill } from "@/types/index";
+
+export const billApi = {
+  // Tạo hóa đơn mới
+  createBill: (data: {
+    contractId: number;
+    month: number;
+    year: number;
+    oldElecIndex: number;
+    newElecIndex: number;
+    oldWaterIndex: number;
+    newWaterIndex: number;
+    deadline?: string;
+  }) => {
+    return axiosClient.post("/bills", data);
+  },
+
+  // Lấy danh sách hóa đơn của 1 hợp đồng
+  getBillsByContract: (contractId: number) => {
+    return axiosClient.get(`/bills/contract/${contractId}`);
+  },
+  getBillingStatus: (month: number, year: number) => {
+    return axiosClient.get(`/bills/billing-status?month=${month}&year=${year}`);
+  }
+};
