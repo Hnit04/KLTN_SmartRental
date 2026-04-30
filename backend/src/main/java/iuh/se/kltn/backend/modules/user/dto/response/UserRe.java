54335b72c4f9e34bf629b70cb03c6fc356d9fc61
Trần Công Tính <165116050+Hnit04@users.noreply.github.com>
2026-04-30 09:49:08 +0700

    .

--- backend/src/main/java/iuh/se/kltn/backend/modules/user/dto/response/UserRe.java
+++ backend/src/main/java/iuh/se/kltn/backend/modules/user/dto/response/UserRe.java
@@ -1,13 +1,9 @@
 package iuh.se.kltn.backend.modules.user.dto.response;
 
-import iuh.se.kltn.backend.common.enums.Role;
-import iuh.se.kltn.backend.modules.user.enums.KYCStatus;
-import jakarta.persistence.Column;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
